c371362b9c83528d6c2f7ffbf294475c0236df87
AbbeyIT <93916892+AbbeyIT@users.noreply.github.com>
2022-06-23 14:48:14 +0800

    add exercise activity in loopings

---
+++ Coding-Exercise/04Loops.py
@@ -0,0 +1,18 @@
+#For Loop
+furnitures = ["table", "chair", "cabinet", "desk", "couch"]
+
+for x in furnitures:
+    if x == "cabinet":
+        continue
+    print(x)
+
+
+#While Loop
+i = 1
+
+while i < 15:
+    print(i)
+    i += 1
+else:
+    print("i is no longer less than 15")
+
